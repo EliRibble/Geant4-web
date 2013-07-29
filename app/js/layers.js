e7b850b2b21bb56cf505443afa1c138bcaa1c4cd
Eli Ribble <eribble@hirevue.com>
2013-07-29 13:41:41 -0600

    Use the correct function call for binding click events

--- app/js/layers.js
+++ app/js/layers.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
 	} else if (numLayers >= 30) {
 		$('#new layer').remove();
 	}
-	$('#add').onClick(function() {
+	$('#add').on('click', function() {
 		numLayers++;
 		$('table').append('<col /><td> Layer {{numLayers}} <label>Pick a material</label><select><option ng-repeat="material in materials | orderBy:orderProp" value="{{material.value}}">{{material.name}}</option></select></td><td>Thickness:<input type="text" ng-model="{number}" required ng-change="{}"/><select><option value="cm">cm</option><option value="km">km</option><option value="m">m</option><option value="mm">mm</option></select></td>');
 		return numLayers;
 		// add a new layer without reloading the site
 	});
-	$('#remove').onClick(function() {
+	$('#remove').on('click', function() {
 		numLayers--;
 		$('table')
 	});
